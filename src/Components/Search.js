0283fa9275f7818b0feb92f5374c67dd739365cb
github username <github email address>
2019-01-24 11:37:44 -0500

    thanks

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,7 +1,10 @@
 import React from "react";
 
-const Search = () => {
-  return <form className="search">{/*Build Form Here*/}</form>;
+const Search = (props) => {
+
+  return (
+  <input className="search" type="text" value={props.value} onChange={(e) => props.handleChange(e)}/>
+)
 };
 
 export default Search;
--- src/Containers/BeerContainer.js
+++ src/Containers/BeerContainer.js
@@ -1,13 +1,41 @@
 import React, { Component } from "react";
-
+import BeerDetail from '../Components/BeerDetail'
+import Search from '../Components/Search'
+import BeerItem from '../Components/BeerItem'
 class BeerContainer extends Component {
+
+state = {
+  beers: [],
+  currentBeer:'',
+  searchedName: ''
+}
+
+
+componentDidMount() {
+  fetch('http://localhost:3001/beers')
+  .then(res => res.json())
+  .then(json => this.setState({beers: json}))
+}
+
+
+
+handleDetail = (beer) => {
+  this.setState({currentBeer: beer})
+}
+
+handleChange = (e) => {
+  this.setState({searchedName: e.target.value, currentBeer: !this.state.currentBeer})
+// changing the state of current beer here is just for design purposes, in order BeerDetail to dissapear when you search:)
+}
   render() {
+
+let filteredBeers = [...this.state.beers].filter(beer => beer.name.toLowerCase().includes(this.state.searchedName.toLowerCase()))
     return (
       <div>
-        <Search />
+        <Search handleChange={this.handleChange} value={this.state.searchedName}/>
         <br />
-        <ul className="container">{/*render Beer Items Here*/}</ul>
-        <BeerCard />
+        <ul className="container">{filteredBeers.map(beer => <BeerItem handleDetail={this.handleDetail} key={beer.id} beer={beer}/>)}</ul>
+         {(this.state.currentBeer) ? <BeerDetail beer={this.state.currentBeer}/> : null}
       </div>
     );
   }
