c34f49c9bb23f597761dc0837193b990c2f3113a
naomijoyce <naomijoyceb@gmail.com>
2019-01-24 11:05:52 -0500

    first deliverable

--- src/Containers/BeerContainer.js
+++ src/Containers/BeerContainer.js
@@ -1,13 +1,43 @@
 import React, { Component } from "react";
+import BeerDetail from '../Components/BeerDetail.js';
+import Search from '../Components/Search.js';
+import BeerItem from '../Components/BeerItem.js';
 
 class BeerContainer extends Component {
+  state ={
+    beer: []
+  }
+
+  componentDidMount(){
+    fetch('http://localhost:3001/beers')
+    .then(response => response.json())
+    // .then(json => console.log(json))
+    .then(data =>
+      this.setState({
+        beer: data
+      })
+    )
+  }
+
+  infoClickHandler = () => {
+    console.log('click');
+  }
+
   render() {
+    console.log(this.state.beer);
     return (
       <div>
         <Search />
         <br />
-        <ul className="container">{/*render Beer Items Here*/}</ul>
-        <BeerCard />
+        <ul className="container">
+          {
+            this.state.beer.map(beer =>
+              <BeerItem key={beer.id} beer={beer} onClick={this.InfoClickHandler}/>
+            )
+          }
+
+        </ul>
+        <BeerDetail />
       </div>
     );
   }
