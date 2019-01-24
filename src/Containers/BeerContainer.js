40c68709c214a2013327740ffcc084053dae8ae3
La Fountain <github email address>
2019-01-24 12:09:15 -0500

    Complete

--- src/Containers/BeerContainer.js
+++ src/Containers/BeerContainer.js
@@ -1,13 +1,52 @@
 import React, { Component } from "react";
+import Search from "../Components/Search";
+import BeerDetail from "../Components/BeerDetail";
 
 class BeerContainer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      beers: []
+    };
+  }
+
+  async componentDidMount() {
+    const response = await fetch("http://localhost:3001/beers");
+    const data = await response.json();
+    this.setState({
+      beers: data,
+      newBeer: ""
+    });
+  }
+
+  handleBeer = e => {
+    e.preventDefault();
+
+    console.log("I work");
+
+    // let result = this.state.beers.filter(beer => beer.id === e.target.id);
+    // console.log(result);
+
+    this.state.beers.filter(beer => {
+      if (beer.id === e.target.id) {
+        this.setState({
+          newBeer: beer
+        });
+      }
+    });
+  };
+
   render() {
     return (
       <div>
         <Search />
-        <br />
-        <ul className="container">{/*render Beer Items Here*/}</ul>
-        <BeerCard />
+        <ul className="container" />
+        {this.state.beers.map(beer => (
+          <li id={beer.id} key={beer.id} onClick={this.handleBeer}>
+            {beer.name}
+          </li>
+        ))}
+        <BeerDetail beers={this.state.newBeer} />
       </div>
     );
   }
