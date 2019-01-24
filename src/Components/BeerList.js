cff7ef38fea65bc27128a5cc1a5e60b6c98e297d
James Lee <jameslee1187@gmail.com>
2019-01-24 11:26:25 -0500

    james and gigi

---
+++ src/Components/BeerList.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import BeerItem from './BeerItem'
+
+class BeerList extends Component {
+
+  render() {
+    let beers=this.props.beers.map(beer=>{
+      return <BeerItem  beer={beer} key={beer.id} handleBeer={this.props.handleBeer}/>
+    })
+    return (
+      <div className="list">
+
+      {beers}
+      </div>
+    );
+  }
+
+}
+
+export default BeerList;
--- src/Containers/BeerContainer.js
+++ src/Containers/BeerContainer.js
@@ -1,13 +1,55 @@
 import React, { Component } from "react";
+import Search from '../Components/Search'
+import BeerDetail from '../Components/BeerDetail'
+import BeerList from '../Components/BeerList'
 
 class BeerContainer extends Component {
+
+  state={
+    beers: [],
+    beer: {},
+    search: [],
+    searchTerm: ''
+  }
+
+  componentDidMount(){
+  fetch("http://localhost:3001/beers")
+  .then(res=>res.json())
+  .then(beers=>{
+    this.setState({
+      beers: beers,
+      search: beers
+
+    })
+  })
+}
+
+  handleBeer=(beer)=>{
+    this.setState({
+      beer: beer
+    })
+  }
+
+  searchBeer=(e)=>{
+    e.preventDefault()
+    let newArr=[...this.state.search].filter(beer=>{
+      return beer.name.toLowerCase().includes(e.target.value.toLowerCase())
+    })
+    this.setState({
+      searchTerm: e.target.value,
+      beers: newArr
+    })
+
+  }
+
   render() {
+
     return (
       <div>
-        <Search />
+        <Search searchTerm={this.state.searchTerm} searchBeer={this.searchBeer}/>
         <br />
-        <ul className="container">{/*render Beer Items Here*/}</ul>
-        <BeerCard />
+        <BeerList  beers={this.state.beers} handleBeer={this.handleBeer}/>
+        <BeerDetail beer={this.state.beer}/>
       </div>
     );
   }
